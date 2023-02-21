8eec8a31be4752dd430883dbbc0b7fae563a8fcc
august-tengland <54542533+august-tengland@users.noreply.github.com>
2023-02-21 16:49:49 +0100

    Feature: Added manual coverage after new tests in HSV-RGB (#69)

--- conversions/rgb_hsv_conversion.py
+++ conversions/rgb_hsv_conversion.py
@@ -6,10 +6,11 @@ green, and blue. Meanwhile, the HSV representation models how colors appear unde
 light. In it, colors are represented using three components: hue, saturation and
 (brightness-)value. This file provides functions for converting colors from one
 representation to the other.
-
 (description adapted from https://en.wikipedia.org/wiki/RGB_color_model and
 https://en.wikipedia.org/wiki/HSL_and_HSV).
 """
+
+cov_array = [False] * 16 # pragma: no cover
 
 
 def hsv_to_rgb(hue: float, saturation: float, value: float) -> list[int]:
@@ -17,7 +18,6 @@ def hsv_to_rgb(hue: float, saturation: float, value: float) -> list[int]:
     Conversion from the HSV-representation to the RGB-representation.
     Expected RGB-values taken from
     https://www.rapidtables.com/convert/color/hsv-to-rgb.html
-
     >>> hsv_to_rgb(0, 0, 0)
     [0, 0, 0]
     >>> hsv_to_rgb(0, 0, 1)
@@ -38,15 +38,36 @@ def hsv_to_rgb(hue: float, saturation: float, value: float) -> list[int]:
     [193, 196, 224]
     >>> hsv_to_rgb(330, 0.75, 0.5)
     [128, 32, 80]
+    >>> hsv_to_rgb(-7, 0.9, 0.11)
+    Traceback (most recent call last):
+        ...
+    Exception: hue should be between 0 and 360
+    >>> hsv_to_rgb(9, 2, 0.11)
+    Traceback (most recent call last):
+        ...
+    Exception: saturation should be between 0 and 1
+    >>> hsv_to_rgb(9, 0.11, 2)
+    Traceback (most recent call last):
+        ...
+    Exception: value should be between 0 and 1
     """
     if hue < 0 or hue > 360:
+        cov_array[0] = True
         raise Exception("hue should be between 0 and 360")
+    else: 
+        cov_array[1] = True    
 
     if saturation < 0 or saturation > 1:
+        cov_array[2] = True
         raise Exception("saturation should be between 0 and 1")
+    else: 
+        cov_array[3] = True
 
     if value < 0 or value > 1:
+        cov_array[4] = True
         raise Exception("value should be between 0 and 1")
+    else: 
+        cov_array[5] = True
 
     chroma = value * saturation
     hue_section = hue / 60
@@ -54,40 +75,53 @@ def hsv_to_rgb(hue: float, saturation: float, value: float) -> list[int]:
     match_value = value - chroma
 
     if hue_section >= 0 and hue_section <= 1:
+        cov_array[6] = True
         red = round(255 * (chroma + match_value))
         green = round(255 * (second_largest_component + match_value))
         blue = round(255 * (match_value))
-    elif hue_section > 1 and hue_section <= 2:
-        red = round(255 * (second_largest_component + match_value))
-        green = round(255 * (chroma + match_value))
-        blue = round(255 * (match_value))
-    elif hue_section > 2 and hue_section <= 3:
-        red = round(255 * (match_value))
-        green = round(255 * (chroma + match_value))
-        blue = round(255 * (second_largest_component + match_value))
-    elif hue_section > 3 and hue_section <= 4:
-        red = round(255 * (match_value))
-        green = round(255 * (second_largest_component + match_value))
-        blue = round(255 * (chroma + match_value))
-    elif hue_section > 4 and hue_section <= 5:
-        red = round(255 * (second_largest_component + match_value))
-        green = round(255 * (match_value))
-        blue = round(255 * (chroma + match_value))
     else:
-        red = round(255 * (chroma + match_value))
-        green = round(255 * (match_value))
-        blue = round(255 * (second_largest_component + match_value))
+        cov_array[7] = True
+        if hue_section > 1 and hue_section <= 2:
+            cov_array[8] = True
+            red = round(255 * (second_largest_component + match_value))
+            green = round(255 * (chroma + match_value))
+            blue = round(255 * (match_value))
+        else:
+            cov_array[9] = True
+            if hue_section > 2 and hue_section <= 3:
+                cov_array[10] = True
+                red = round(255 * (match_value))
+                green = round(255 * (chroma + match_value))
+                blue = round(255 * (second_largest_component + match_value))
+            else:
+                cov_array[11] = True 
+                if hue_section > 3 and hue_section <= 4:
+                    cov_array[12] = True
+                    red = round(255 * (match_value))
+                    green = round(255 * (second_largest_component + match_value))
+                    blue = round(255 * (chroma + match_value))
+                else:
+                    cov_array[13] = True
+                    if hue_section > 4 and hue_section <= 5:
+                        cov_array[14] = True
+                        red = round(255 * (second_largest_component + match_value))
+                        green = round(255 * (match_value))
+                        blue = round(255 * (chroma + match_value))
+                    else:
+                        cov_array[15] = True
+                        red = round(255 * (chroma + match_value))
+                        green = round(255 * (match_value))
+                        blue = round(255 * (second_largest_component + match_value))
 
     return [red, green, blue]
 
 
-def rgb_to_hsv(red: int, green: int, blue: int) -> list[float]:
+def rgb_to_hsv(red: int, green: int, blue: int) -> list[float]: # pragma: no cover
     """
     Conversion from the RGB-representation to the HSV-representation.
     The tested values are the reverse values from the hsv_to_rgb-doctests.
     Function "approximately_equal_hsv" is needed because of small deviations due to
     rounding for the RGB-values.
-
     >>> approximately_equal_hsv(rgb_to_hsv(0, 0, 0), [0, 0, 0])
     True
     >>> approximately_equal_hsv(rgb_to_hsv(255, 255, 255), [0, 0, 1])
@@ -139,10 +173,9 @@ def rgb_to_hsv(red: int, green: int, blue: int) -> list[float]:
     return [hue, saturation, value]
 
 
-def approximately_equal_hsv(hsv_1: list[float], hsv_2: list[float]) -> bool:
+def approximately_equal_hsv(hsv_1: list[float], hsv_2: list[float]) -> bool: # pragma: no cover
     """
     Utility-function to check that two hsv-colors are approximately equal
-
     >>> approximately_equal_hsv([0, 0, 0], [0, 0, 0])
     True
     >>> approximately_equal_hsv([180, 0.5, 0.3], [179.9999, 0.500001, 0.30001])
@@ -157,3 +190,19 @@ def approximately_equal_hsv(hsv_1: list[float], hsv_2: list[float]) -> bool:
     check_value = abs(hsv_1[2] - hsv_2[2]) < 0.002
 
     return check_hue and check_saturation and check_value
+
+
+if __name__ == "__main__": # pragma: no cover
+    hsv_to_rgb(0, 0, 0)
+    hsv_to_rgb(0, 0, 1)
+    hsv_to_rgb(0, 1, 1)
+    hsv_to_rgb(60, 1, 1)
+    hsv_to_rgb(120, 1, 1)
+    hsv_to_rgb(240, 1, 1)
+    hsv_to_rgb(300, 1, 1)
+    hsv_to_rgb(180, 0.5, 0.5)
+    hsv_to_rgb(234, 0.14, 0.88)
+    hsv_to_rgb(330, 0.75, 0.5)
+
+    print("branch coverage is: " + str(sum(cov_array) / len(cov_array) * 100) + "%")
+    
